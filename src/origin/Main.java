88c0da93f1f6440dab0e114742cce28c3047bfd5
vine <>
2019-04-11 00:02:25 +0900

    first commit

--- src/origin/Main.java
+++ src/origin/Main.java
@@ -2,9 +2,12 @@ package origin;
 
 import peasy.PeasyCam;
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class Main extends PApplet {
     PeasyCam cam;
+    PVector [][] globe;
+    int total = 20;
 
     public void settings(){
         size(600,600, P3D);
@@ -12,31 +15,43 @@ public class Main extends PApplet {
 
     public void setup(){
         cam = new PeasyCam(this,500);
+        globe = new PVector[total+1][total+1];
     }
 
     public void draw(){
         background(0);
-        fill(255);
+
         lights();
         //translate(width/2,height/2);
         //sphere(200);
 
         float r = 200;
-        int total = 20;
-        for(int i = 0; i < 100; i++){
-            float lon = map(i, 0, total, -PI, PI);
-            for(int j = 0; j < 100; j++){
-                float lat = map(j, 0, total, -HALF_PI, HALF_PI);
+
+        for(int i = 0; i < total+1; i++){
+            float lat = map(i, 0, total, -HALF_PI, HALF_PI);
+            for(int j = 0; j < total+1; j++){
+                float lon = map(j, 0, total, -PI, PI);
 
                 float x = r * sin(lon) * cos(lat);
                 float y = r * sin(lon) * sin(lat);
                 float z = r * cos(lon);
-                stroke(255);
-                strokeWeight(4);
-                point(x,y,z);
+                globe[i][j] = new PVector(x,y,z);
             }
         }
 
+        for(int i = 0; i < total; i++){
+            beginShape(TRIANGLE_STRIP);
+            for(int j = 0; j < total+1; j++){
+                PVector v1 = globe[i][j];
+                stroke(255);
+                strokeWeight(2);
+                vertex(v1.x, v1.y, v1.z);
+                PVector v2 = globe[i+1][j];
+                vertex(v2.x, v2.y, v2.z);
+
+            }
+            endShape();
+        }
 
     }
 
