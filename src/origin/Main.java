fca6162dafe0d0b65c26a4eef837f3f4a231a49a
vine <>
2019-04-17 22:20:59 +0900

    physics.add()を追加しても描画するところまで確認。globeはnodesに置き換えて良い？

--- src/origin/Main.java
+++ src/origin/Main.java
@@ -11,16 +11,21 @@ import toxi.physics3d.VerletSpring3D;
 public class Main extends PApplet {
 
     VerletPhysics3D physics;
+    Jelly j;
 
     PImage loadImg;
     PShape halfSphere;
 
 
     PeasyCam cam;
-    PVector [] globe;
-    PVector [] plain;
+    //PVector [] globe, plain;
+    PVector[] plain;
+    Vec3D[] globe;
     int total = 20;
     int[] vertexes2;
+
+    Node[] nodes;
+    Connection[] connections;
 
 
     public void settings(){
@@ -37,21 +42,10 @@ public class Main extends PApplet {
         cam = new PeasyCam(this,500);
 
         vertexes2 = new int[total * total * 4];
-        globe = new PVector[total * total * 4];
+        globe = new Vec3D[total * total * 4];
         plain = new PVector[total * total * 4];
 
 
-    }
-
-
-    public void draw(){
-        physics.update();
-
-        background(0);
-
-        lights();
-        //translate(width/2,height/2);
-        //sphere(200);
 
         //球の定義：ここから
         halfSphere = createShape();
@@ -72,7 +66,7 @@ public class Main extends PApplet {
             float y = r * sin(lon) * sin(lat);
             float z = r * cos(lon);
             int j = vertexes2[i];
-            globe[j] = new PVector(x,y,z);
+            globe[j] = new Vec3D(x,y,z);
             float v = map(floor(vertexes2[i]/(total+1)),0, total, 0,loadImg.height);
             float u = map(vertexes2[i] % (total+1),0, total,0, loadImg.width);
             plain[j] = new PVector(u,v);
@@ -87,20 +81,21 @@ public class Main extends PApplet {
             halfSphere.vertex(globe[j].x, globe[j].y, globe[j].z, plain[j].x, plain[j].y);
         }
         halfSphere.endShape();
-
         //球の定義：ここまで
 
-        image(loadImg,0,0);
-        pushMatrix();
-        rotateX(HALF_PI);
-        rotateY(HALF_PI);
-        shape(halfSphere);
-        popMatrix();
 
-        //drawSoratama();
+        j = new Jelly();
+    }
 
-        //testSphere.setTexture(loadImg);
-        //shape(testSphere);
+
+    public void draw(){
+        physics.update();
+
+        background(0);
+
+        lights();
+        //translate(width/2,height/2);
+        j.display();
 
         //noLoop();
     }
@@ -117,6 +112,79 @@ public class Main extends PApplet {
         Connection(Node n1, Node n2, float len, float strength){
             super(n1, n2, len, strength);
         }
+    }
+
+    public class Jelly {
+        Node[] nodes;
+        //Connection[] connections;
+
+        Jelly() {
+
+            nodes = new Node[total * total * 4];
+            connections = new Connection[total * total * 4];
+            float strength = 0.1F;
+
+            for (int i = 0; i < vertexes2.length; i += 4) {
+                int j = vertexes2[i];
+                nodes[j] = new Node(globe[j]);
+                int k = vertexes2[i + 1];
+                nodes[k] = new Node(globe[k]);
+                int l = vertexes2[i + 2];
+                nodes[l] = new Node(globe[l]);
+                int m = vertexes2[i + 3];
+                nodes[m] = new Node(globe[m]);
+
+                physics.addParticle(nodes[j]);
+                physics.addParticle(nodes[k]);
+                physics.addParticle(nodes[l]);
+                physics.addParticle(nodes[m]);
+
+                Vec3D left = nodes[k].sub(nodes[j]);
+                float dleft = left.magnitude();
+                Connection sleft = new Connection(nodes[j], nodes[k], dleft, strength);
+                Vec3D down = nodes[m].sub(nodes[j]);
+                float ddown = down.magnitude();
+                Connection sdown = new Connection(nodes[j], nodes[m], ddown, strength);
+
+                physics.addSpring(sleft);
+                physics.addSpring(sdown);
+
+                if(k % total == 0){
+                    Vec3D down2 = nodes[l].sub(nodes[k]);
+                    float ddown2 = down2.magnitude();
+                    Connection sdown2 = new Connection(nodes[k], nodes[l], ddown2, strength);
+                    physics.addSpring(sdown2);
+                }
+
+                if(m % total == 0){
+                    Vec3D left2 = nodes[l].sub(nodes[m]);
+                    float dleft2 = left2.magnitude();
+                    Connection sleft2 = new Connection(nodes[l], nodes[m], dleft2, strength);
+                    physics.addSpring(sleft2);
+                }
+            }
+        }
+
+        void display(){
+
+                halfSphere.beginShape(QUADS);
+                for (int i = 0; i < vertexes2.length; i++) {
+                    if (i % 4 == 0) {
+                        halfSphere.texture(loadImg);
+                    }
+                    int j = vertexes2[i];
+                    halfSphere.vertex(nodes[j].x, nodes[j].y, nodes[j].z, plain[j].x, plain[j].y);
+                }
+                halfSphere.endShape();
+
+
+                pushMatrix();
+                rotateX(HALF_PI);
+                rotateY(HALF_PI);
+                shape(halfSphere);
+                popMatrix();
+        }
+
     }
 
 
