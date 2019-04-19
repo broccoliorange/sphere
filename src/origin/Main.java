ec7d280780bf961e8615afc3172fcaa21e4a041f
vine <>
2019-04-19 23:41:54 +0900

    attractorはsetup()でなくdraw()の中に書く。ここから動きのデザイン

--- src/origin/Main.java
+++ src/origin/Main.java
@@ -19,6 +19,8 @@ public class Main extends PApplet {
     PImage loadImg;
 
     Attractor attractor;
+    float aStrength;
+    float angle;
 
     //PeasyCam cam;
 
@@ -36,7 +38,7 @@ public class Main extends PApplet {
         loadImg = loadImage("colorbar.jpg");
 
         j = new Jelly();
-        attractor = new Attractor( new Vec3D(0, 0, 0));
+
 
         //cam = new PeasyCam(this,500);
     }
@@ -45,17 +47,17 @@ public class Main extends PApplet {
     public void draw(){
         background(0);
         physics.update();
+        attractor = new Attractor( new Vec3D(0, 0, 0), aStrength);
 
         lights();
         translate(width/2,height/2);
         j.display();
 
-        //if (mousePressed) {
-        //    attractor.lock();
-        //    attractor.x += 0.01;
-        //} else {
-        //    attractor.unlock();
-        //}
+        aStrength = -0.1F;
+        //aStrength = map(sin(angle), -1,1,-0.001F, 0.001F);
+        //angle += 0.01;
+
+
     }
 
     public class Node extends VerletParticle3D{
@@ -74,13 +76,13 @@ public class Main extends PApplet {
 
     public class Attractor extends VerletParticle3D{
 
-        float r;
+        float aStrength;
 
-        Attractor (Vec3D loc){
+        Attractor (Vec3D loc, float aStrength){
             super(loc);
-            r=10;
+            this.aStrength = aStrength;
             physics.addParticle(this);
-            physics.addBehavior(new AttractionBehavior3D(this,width, 0.01F));
+            physics.addBehavior(new AttractionBehavior3D(this,width, aStrength));
         }
 
         void display(){
@@ -104,19 +106,19 @@ public class Main extends PApplet {
             plain = new PVector[total * total * 4];
             nodes = new Node[total * total * 4];
             connections = new ArrayList<Connection>();
-            float strength = 0.9F;
+            float cStrength = 0.95F;
 
-            //半球の定義：ここから
-
+            //頂点の定義(CW)
             float r = 200;
 
             for (int i = 0, j = 0; i < vertexes2.length; i += 4, j++) {
-                vertexes2[i + 0] = j + floor(j / total);                  //CW
+                vertexes2[i + 0] = j + floor(j / total);
                 vertexes2[i + 1] = j + floor(j / total) + 1;
                 vertexes2[i + 2] = j + floor(j / total) + 2 + total;
                 vertexes2[i + 3] = j + floor(j / total) + 1 + total;
             }
 
+            //半頂点番号→球表面の座標、テクスチャ画像の座標
             for(int i = 0; i < vertexes2.length; i++){
                 float lat = map(floor(vertexes2[i]/(total+1)), 0, total, 0, PI);
                 float lon = map(vertexes2[i] % (total+1),0, total,0, PI);
@@ -130,8 +132,8 @@ public class Main extends PApplet {
                 float u = map(vertexes2[i] % (total+1),0, total,0, loadImg.width);
                 plain[j] = new PVector(u,v);
             }
-            //半球の定義：ここまで
 
+            //半球頂点とノード、ノード間コネクションの関連付け、半球切り口のロック
             for(int j = 0; j < (total+1)*(total+1); j++){
                 physics.addParticle( nodes[j]);
             }
@@ -144,41 +146,40 @@ public class Main extends PApplet {
 
                 Vec3D left = nodes[k].sub(nodes[j]);
                 float dleft = left.magnitude();
-                Connection sleft = new Connection(nodes[j], nodes[k], dleft, strength);
+                Connection sleft = new Connection(nodes[j], nodes[k], dleft, cStrength);
                 Vec3D down = nodes[m].sub(nodes[j]);
                 float ddown = down.magnitude();
-                Connection sdown = new Connection(nodes[j], nodes[m], ddown, strength);
+                Connection sdown = new Connection(nodes[j], nodes[m], ddown, cStrength);
 
                 physics.addSpring(sleft);
                 connections.add(sleft);
                 physics.addSpring(sdown);
                 connections.add(sdown);
 
-                if(k % total == 0){
+                if(j >= 0 && j < total || j % (total +1) == 0){
+                    nodes[j].lock();
+                }
+
+                if((k + 1) % (total + 1) == 0){
                     Vec3D down2 = nodes[l].sub(nodes[k]);
                     float ddown2 = down2.magnitude();
-                    Connection sdown2 = new Connection(nodes[k], nodes[l], ddown2, strength);
+                    Connection sdown2 = new Connection(nodes[k], nodes[l], ddown2, cStrength);
                     physics.addSpring(sdown2);
                     connections.add(sdown2);
                     nodes[k].lock();
                 }
 
-                if(m % total == 0){
+                if(m >= total*(total + 1)){
                     Vec3D left2 = nodes[l].sub(nodes[m]);
                     float dleft2 = left2.magnitude();
-                    Connection sleft2 = new Connection(nodes[l], nodes[m], dleft2, strength);
+                    Connection sleft2 = new Connection(nodes[l], nodes[m], dleft2, cStrength);
                     physics.addSpring(sleft2);
                     connections.add(sleft2);
                     nodes[m].lock();
                 }
 
-                if(j >= 0 && j < total || j % (total +1) == 0){
-                    nodes[j].lock();
-                }
+                nodes[(total + 1)*(total +1)-1].lock();
 
-                if(k % total == 0 && m % total == 0){
-                    nodes[l].lock();
-                }
             }
         }
 
