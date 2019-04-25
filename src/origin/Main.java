b037a737171afbaffc121b26786f767dd6f265a6
vine <>
2019-04-25 11:41:43 +0900

    継ぎ目のテクスチャマップが変。球の逃げはロックノードの工夫でしのぐ

--- src/origin/Main.java
+++ src/origin/Main.java
@@ -49,13 +49,13 @@ public class Main extends PApplet {
         background(0);
         physics.update();
 
-        attractor = new Attractor( new Vec3D(0, 0, 0), aStrength);
+        attractor = new Attractor( new Vec3D(ax, ay, 0), aStrength);
         ax += random(5);
         ay += random(5);
-        aStrength = -0.0001F;
-        //aStrength = map(sin(angle), -1,1,0.0F, 0.1F);
-        //angle += HALF_PI/1000;
-        //attractor.lock();
+        //aStrength = -0.0001F;
+        aStrength = map(sin(angle), -1,1,-0.0001F, 0.0F);
+        angle += HALF_PI/1000;
+        attractor.lock();
         lights();
         //translate(width/2,height/2);
         j.display();
@@ -74,26 +74,10 @@ public class Main extends PApplet {
 
 
     public  class  Connection extends VerletConstrainedSpring3D {
-        float x1,y1,z1,x2,y2,z2;
 
         Connection(Node n1, Node n2, float len, float strength){
 
             super(n1, n2, len, strength);
-
-            x1 = n1.x;
-            y1 = n1.y;
-            z1 = n1.z;
-            x2 = n2.x;
-            y2 = n2.y;
-            z2 = n2.z;
-        }
-
-        void display(){
-            beginShape();
-            stroke(255);
-            vertex(x1,y1,z1);
-            vertex(x2,y2,z2);
-            endShape();
         }
     }
 
@@ -117,7 +101,7 @@ public class Main extends PApplet {
 
         PShape halfSphere;
 
-        int total = 7;
+        int total = 20;
         int[] vertexes2;
         PVector[] plain;
         Node[] nodes;
@@ -129,9 +113,9 @@ public class Main extends PApplet {
             plain = new PVector[total * total * 4];
             nodes = new Node[total * total * 4];
             connections = new ArrayList<Connection>();
-            float cStrength = 0.1F;
+            float cStrength = 0.5F;
 
-            //頂点の定義(CW)
+            //頂点番号の並び方の定義(CW　右端＝左端)
             float r = 200;
 
             for (int i = 0, j = 0; i < vertexes2.length; i += 4, j++) {
@@ -139,9 +123,17 @@ public class Main extends PApplet {
                 vertexes2[i + 1] = j + floor(j / total) + 1;
                 vertexes2[i + 2] = j + floor(j / total) + 2 + total;
                 vertexes2[i + 3] = j + floor(j / total) + 1 + total;
+                if((vertexes2[i+1]+1) % (total+1) == 0){
+                    vertexes2[i+1] -= total;
+                }
+
+                if((vertexes2[i+2]+1) % (total+1) == 0){
+                    vertexes2[i+2] -= total;
+                }
             }
 
-            //頂点番号→球表面の座標、テクスチャ画像の座標
+
+            //頂点番号と球表面の座標、テクスチャ画像の座標の関連づけ
             for(int i = 0; i < vertexes2.length; i++){
                 float lat = map(floor(vertexes2[i]/(total+1)), 0, total, 0, PI);
                 float lon = map(vertexes2[i] % (total+1),0, total,0, TWO_PI);
@@ -156,30 +148,27 @@ public class Main extends PApplet {
                 plain[j] = new PVector(u,v);
             }
 
-            //球頂点とノード、ノード間コネクションの関連付け
+            //ノードを物理系へ登録
             for(int j = 0; j < (total+1)*(total+1); j++){
-                physics.addParticle( nodes[j]);
-
+                if( !((j+1)%(total+1)==0)){
+                    physics.addParticle( nodes[j]);
+                }
             }
 
+            //コネクションを生成、物理系へ登録
             for (int i = 0; i < vertexes2.length; i += 4) {
                 int j = vertexes2[i];
                 int k = vertexes2[i + 1];
                 int l = vertexes2[i + 2];
                 int m = vertexes2[i + 3];
-
-                if((k + 1) % (total + 1) == 0){
-                    nodes[k] = nodes[k - total];
-                    nodes[l] = nodes[l - total];
-                }
+                if((k+1)%(total+1)==0){ k-=total; }
+                if((l+1)%(total+1)==0){ l-=total; }
 
                 Vec3D left = nodes[k].sub(nodes[j]);
                 float dleft = left.magnitude();
                 Connection sleft = new Connection(nodes[j], nodes[k], dleft, cStrength);
                 physics.addSpring(sleft);
                 connections.add(sleft);
-                println("j=",j," k=",k,"nodes:",nodes[j],nodes[k]);
-                sleft.display();
 
                 Vec3D down = nodes[m].sub(nodes[j]);
                 float ddown = down.magnitude();
@@ -201,6 +190,8 @@ public class Main extends PApplet {
                     connections.add(sleftbtm);
                 }
 
+             nodes[floor(total*total/2)].lock();
+
             }
         }
 
@@ -216,9 +207,7 @@ public class Main extends PApplet {
                         halfSphere.texture(loadImg);
                     }
                     int j = vertexes2[i];
-                    //if((j + 1) % (total +1) == 0){
-                    //    j = j - total;
-                    //}
+                    if((j + 1) % (total + 1) == 0){ j -= total; }
                     halfSphere.vertex(nodes[j].x, nodes[j].y, nodes[j].z, plain[j].x, plain[j].y);
                 }
                 halfSphere.endShape();
