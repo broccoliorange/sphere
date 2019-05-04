bacd3e66123d8400d9af79270044ac8b44ead7b7
vine <>
2019-05-04 21:39:05 +0900

    継ぎ目のテクスチャマップが変。旧来の円形マスク法で球の端をぼかす

--- src/origin/Main.java
+++ src/origin/Main.java
@@ -4,8 +4,6 @@ import peasy.PeasyCam;
 import processing.core.*;
 import toxi.geom.Vec3D;
 import toxi.physics3d.*;
-import toxi.physics3d.behaviors.AttractionBehavior3D;
-
 import java.util.ArrayList;
 
 
@@ -14,16 +12,21 @@ public class Main extends PApplet {
     VerletPhysics3D physics;
     JellyBall jb;
 
+    //float r;
+
     int fc = 0;
 
-    PImage loadImg;
+    PImage backImg;
+    PImage blurImg;
+    PImage texImg;
+    PGraphics maskImg; //仕上げスムージングのためのマスク
 
     PeasyCam cam;
 
 
 
     public void settings(){
-        size(1024,1024, P3D);
+        size(1080,578, P3D);
     }
 
     public void setup(){
@@ -31,11 +34,24 @@ public class Main extends PApplet {
 
         imageMode(CENTER);
 
-        loadImg = loadImage("gaju1024.jpg");
+        backImg = loadImage("colorbar.jpg");
+        backImg.filter(BLUR,8);
+        texImg = loadImage("gaju1024.jpg");
+
+
+        image(backImg,0,0);
 
         jb = new JellyBall();
 
-        cam = new PeasyCam(this,500);
+        //r = 120;
+
+        maskImg=createGraphics(width,height,P2D);
+        maskImg.beginDraw();
+        maskImg.fill(0);
+        maskImg.ellipse(width/2,height/2,120*2*0.9F,120*2*0.9F);
+        maskImg.endDraw();
+
+        //cam = new PeasyCam(this,500);
     }
 
 
@@ -44,8 +60,8 @@ public class Main extends PApplet {
         physics.update();
 
 
-        //lights();
-        //translate(width/2,height/2);
+        lights();
+        translate(width/2,height/2);
         jb.display();
         if(fc % 100==0){
             jb.squeeze();
@@ -104,7 +120,6 @@ public class Main extends PApplet {
             float cStrength2 = 0.1F;
 
             //頂点番号の並び方の定義(CW　右端＝左端)
-            float r = 120;
 
             for (int i = 0, j = 0; i < vertexes2.length; i += 4, j++) {
                 vertexes2[i + 0] = j + floor(j / total);
@@ -122,6 +137,8 @@ public class Main extends PApplet {
 
 
             //頂点番号と球表面の座標、テクスチャ画像の座標の関連づけ
+
+            float r = 120;
             for(int i = 0; i < vertexes2.length; i++){
                 float lat = map(floor(vertexes2[i]/(total+1)), 0, total, 0, PI);
                 float lon = map(vertexes2[i] % (total+1),0, total,0, TWO_PI);
@@ -131,8 +148,8 @@ public class Main extends PApplet {
                 int j = vertexes2[i];
                 nodes[j] = new Node( new Vec3D(x,y,z));
 
-                float v = map(floor(vertexes2[i]/(total+1)),0, total, 0,loadImg.height);
-                float u = map(vertexes2[i] % (total+1),0, total,0, loadImg.width);
+                float v = map(floor(vertexes2[i]/(total+1)),0, total, 0,texImg.height);
+                float u = map(vertexes2[i] % (total+1),0, total,0, texImg.width);
                 plain[j] = new PVector(u,v);
             }
 
@@ -201,13 +218,14 @@ public class Main extends PApplet {
         }
 
         void display(){
+            image(backImg,0,0);
 
             ball = createShape();
 
             ball.beginShape(QUADS);
             for (int i = 0; i < vertexes2.length; i++) {
                 if (i % 4 == 0) {
-                    ball.texture(loadImg);
+                    ball.texture(texImg);
                 }
                 int j = vertexes2[i];
                 if((j + 1) % (total + 1) == 0){ j -= total; }
@@ -215,14 +233,16 @@ public class Main extends PApplet {
             }
             ball.endShape();
 
-            //strokeWeight(0);
+            strokeWeight(0);
             pushMatrix();
             rotateZ(HALF_PI);
             rotateY(HALF_PI);
             shape(ball);
             popMatrix();
-        }
 
+            mask(maskImg);
+            filter(BLUR,2);
+        }
 
         void squeeze(){
             //赤道を絞ってみる
@@ -231,7 +251,7 @@ public class Main extends PApplet {
                     nodes[j].lock();
                     Vec3D v = nodes[j].getNormalized();
                     v.scaleSelf(map(abs(v.z),0,1,-50,0));
-                    v.rotateZ( map(v.z,-1,1,-0.05F,0.05F) );
+                    v.rotateZ( map(v.z,-1,1,-0.08F,0.08F) );
                     v.setZ(0);
                     nodes[j].addSelf(v);
                     nodes[j].unlock();
